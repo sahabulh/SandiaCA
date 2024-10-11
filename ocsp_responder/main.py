53540f0b52240ff1b4841670c77220892e08d0eb
Md Sahabul Hossain <sahabulh@iut-dhaka.edu>
2024-10-10 19:04:42 -0600

    Updated OCSP responder with MongoDB
    Signed-off-by: Md Sahabul Hossain <sahabulh@iut-dhaka.edu>

--- ocsp_responder/main.py
+++ ocsp_responder/main.py
@@ -4,16 +4,34 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).absolute().parent.parent))
 
 from fastapi import FastAPI, Request, Response
+
+from cryptography.x509 import ocsp
 from cryptography.x509.ocsp import load_der_ocsp_request, OCSPCertStatus, OCSPResponseStatus
 from cryptography.hazmat.primitives import hashes, serialization
-from cryptography.x509 import ocsp
+from cryptography.hazmat.primitives.asymmetric import ec
+
+from contextlib import asynccontextmanager
+
+from pymongo import MongoClient
 
 import utils
-from exceptions import SHA1Error, WrongIssuerError, ResponseNotAllowedError
+from exceptions import SHA1Error, WrongIssuerError, ResponseNotAllowedError, EntryNotFoundError
 
-app = FastAPI()
+# Define the database variable
+sandia_ca = None
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    global sandia_ca
+    # Connect to MongoDB
+    mongodb_client = MongoClient("localhost",27017, serverSelectionTimeoutMS=10, connectTimeoutMS=1000)
+    sandia_ca = mongodb_client.sandia_ca
+    await utils.load_db(sandia_ca)
+    yield
+    # Disconnect from MongoDB
+    mongodb_client.close()
 
+app = FastAPI(lifespan=lifespan)
 
 @app.post("/")
 async def post_method_process(request: Request):
@@ -40,18 +58,16 @@ async def get_response(req: ocsp.OCSPRequest):
     builder = ocsp.OCSPResponseBuilder()
 
     try:
-        with open("../cert_db.json","r") as file:
-            cert_db = json.load(file)
-        with open("../ocsp_db.json","r") as file:
-            ocsp_db = json.load(file)
         try:
-            issuer_serial = str(cert_db[target_serial]["issuer"])
-            ocsp_serial = str(ocsp_db[issuer_serial])
-        except KeyError:
+            target_cert_info = await utils.get_cert_info(serial=target_serial)
+            issuer_serial = target_cert_info.issuer
+            issuer_cert_info = await utils.get_cert_info(serial=issuer_serial)
+            ocsp_serial = issuer_cert_info.responder
+        except EntryNotFoundError:
             raise ResponseNotAllowedError("Information not available in the database. This might mean that the responder is not authorized to send a response for this certificate.")
         
-        target_cert = await utils.load_cert(target_serial)
-        issuer_cert = await utils.load_cert(issuer_serial)
+        target_cert = utils.load_cert(target_serial)
+        issuer_cert = utils.load_cert(issuer_serial)
 
         req_builder = ocsp.OCSPRequestBuilder()
         req_builder = req_builder.add_certificate(target_cert, issuer_cert, req.hash_algorithm)
@@ -61,14 +77,14 @@ async def get_response(req: ocsp.OCSPRequest):
             raise WrongIssuerError()
 
         responder_cert, responder_key = utils.load_cert_and_key(ocsp_serial)
-        if cert_db[target_serial]["revocation_time"]:
-            revocation_time = datetime.datetime.fromisoformat(cert_db[target_serial]["revocation_time"])
+        if target_cert_info.revocation_time:
+            revocation_time = datetime.datetime.fromisoformat(target_cert_info.revocation_time)
         else:
             revocation_time = None
         
         builder = builder.add_response(
             cert=target_cert, issuer=issuer_cert, algorithm=req.hash_algorithm,
-            cert_status=OCSPCertStatus(cert_db[target_serial]["status"]),
+            cert_status=OCSPCertStatus(target_cert_info.status),
             this_update=datetime.datetime.now(),
             next_update=datetime.datetime.now() + datetime.timedelta(1,0,0),
             revocation_time=revocation_time, revocation_reason=None
@@ -77,7 +93,10 @@ async def get_response(req: ocsp.OCSPRequest):
         ).responder_id(
             ocsp.OCSPResponderEncoding.HASH, responder_cert
         )
-        response = builder.sign(responder_key, hashes.SHA256())
+        if isinstance(responder_key, ec.EllipticCurvePrivateKey):
+            response = builder.sign(responder_key, hashes.SHA256())
+        else:
+            response = builder.sign(responder_key, None)
         res = response.public_bytes(encoding=serialization.Encoding.DER)
     except (ResponseNotAllowedError, SHA1Error, WrongIssuerError) as err:
         res = bad_status(err, OCSPResponseStatus.UNAUTHORIZED)
